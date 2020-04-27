acaa4b47ddb1a971d2c34b77daee26ddd26bab81
Yaekith <yaekith@gmail.com>
2020-04-27 18:19:17 +0800

    Update Console.h

--- Console.h
+++ Console.h
@@ -3,15 +3,8 @@
 #include <iostream>
 
 struct Console {
-	static void Consolebypass() {
-		DWORD CheckerValue;
-		DWORD* OrginalValue = &CheckerValue;
-		VirtualProtect((PVOID)&FreeConsole, 1, PAGE_EXECUTE_READWRITE, OrginalValue);
-		*(BYTE*)(&FreeConsole) = (0xC3);
-	}
 	static void ShowConsole(const char* title)
 	{
-		Consolebypass();
 		AllocConsole();
 		freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
 		freopen_s((FILE**)stdin, "CONIN$", "r", stdin);
